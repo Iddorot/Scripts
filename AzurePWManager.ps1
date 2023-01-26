a5cd00653335223bac5b44ac310655a0da828038
Ido Rot <89927111+Iddorot@users.noreply.github.com>
2023-01-26 13:44:46 +0000

    Add files via upload

---
+++ AzurePWManager.ps1
@@ -0,0 +1,17 @@
+﻿# Log in to Azure
+az login
+
+# Select Azure subscription
+$subscriptionId = Read-Host "Enter your Azure subscription ID"
+az account set --subscription $subscriptionId
+
+# Get list of key vault secrets
+$vaultName = Read-Host "Enter the name of the key vault"
+az keyvault secret list --vault-name $vaultName -o table --query "[].{name:name}"
+
+# Prompt user to select a secret
+$secretName = Read-Host "Enter the name of the secret you want to retrieve"
+
+# Retrieve the secret value and copy to clipboard
+az keyvault secret show --name $secretName --vault-name $vaultName --query "value" -o tsv | clip
+Write-Host "the password was copied to your clipboard"
---
+++ WifiActionNeeded.ps1
@@ -0,0 +1,32 @@
+﻿
+function Change-RegistryKey {
+    param(
+        [string]$registryPath,
+        [string]$registryKey,
+        [int]$newValue
+    )
+
+    try {
+        if( (Get-ItemProperty -Path $registryPath -Name $registryKey) -eq $null ){
+            # Create new registry key
+            New-ItemProperty -Path $registryPath -Name $registryKey -PropertyType DWORD -Value $newValue -Force
+            $message = "Registry key $registryKey created at $registryPath with value $newValue"
+        }else{
+            # Change registry key value
+            Set-ItemProperty -Path $registryPath -Name $registryKey -Value $newValue
+            $message = "Registry key $registryKey at $registryPath has been updated with new value $newValue"
+        }
+        # write log to file
+        $logFile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\WifiActionNeededLog.txt"
+        Add-Content -Path $logFile -Value $message
+    } catch {
+        #write error message to log file
+        $errorMessage = "Error: $($_.Exception.Message)"
+        $logFile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\WifiActionNeededLog.txt"
+        Add-Content -Path $logFile -Value $errorMessage
+    }
+}
+
+Change-RegistryKey -registryPath "HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet" -registryKey "EnableActiveProbing" -newValue 0
+Change-RegistryKey -registryPath "HKLM:\Software\Policies\Microsoft\Windows\NetworkConnectivityStatusIndicator" -registryKey "NoActiveProbe" -newValue 1
+Change-RegistryKey -registryPath "HKLM:\Software\Policies\Microsoft\Windows\NetworkConnectivityStatusIndicator" -registryKey "DisablePassivePolling" -newValue 1
