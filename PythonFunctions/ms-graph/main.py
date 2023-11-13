c170803d68ab0b692d4d7621efe89e7aa46ee217
Ido Rot <89927111+Iddorot@users.noreply.github.com>
2023-11-13 09:18:19 +0000

    add invite_guest_user

--- PythonFunctions/ms-graph/main.py
+++ PythonFunctions/ms-graph/main.py
@@ -3,25 +3,22 @@ import configparser
 from msgraph.generated.models.o_data_errors.o_data_error import ODataError
 from graph import Graph
 
-async def main():
-    print('Python Graph Tutorial\n')
 
+async def main():
     # Load settings
     config = configparser.ConfigParser()
-    config.read(['config.cfg', 'config.dev.cfg'])
-    azure_settings = config['azure']
+    config.read(["config.cfg", "config.dev.cfg"])
+    azure_settings = config["azure"]
 
     graph: Graph = Graph(azure_settings)
-
-    await greet_user(graph)
 
     choice = -1
 
     while choice != 0:
-        print('Please choose one of the following options:')
-        print('0. Exit')
-        print('1. Display access token')
-        print('2. Make a Graph call')
+        print("Please choose one of the following options:")
+        print("0. Exit")
+        print("1. Display access token")
+        print("2. Invite a guest user")
 
         try:
             choice = int(input())
@@ -30,29 +27,28 @@ async def main():
 
         try:
             if choice == 0:
-                print('Goodbye...')
+                print("Goodbye...")
             elif choice == 1:
                 await display_access_token(graph)
             elif choice == 2:
-                await make_graph_call(graph)
+                await invite_guest_user(graph)
             else:
-                print('Invalid choice!\n')
+                print("Invalid choice!\n")
         except ODataError as odata_error:
-            print('Error:')
+            print("Error:")
             if odata_error.error:
                 print(odata_error.error.code, odata_error.error.message)
-async def greet_user(graph: Graph):
-    # TODO
-    return
+
 
 async def display_access_token(graph: Graph):
     token = await graph.get_user_token()
-    print('User token:', token, '\n')
+    print("User token:", token, "\n")
 
 
-async def make_graph_call(graph: Graph):
-    # TODO
-    return
+async def invite_guest_user(graph: Graph):
+    response = await graph.invite_guest_user()
+    print(response, "\n")
+
 
 # Run main
 asyncio.run(main())
