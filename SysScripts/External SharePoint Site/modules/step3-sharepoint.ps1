c0798e17dfbaefb9f7af7def476734a5b5515de4
Ido Rot <89927111+Iddorot@users.noreply.github.com>
2026-05-17 17:06:04 +0300

    Update step3-sharepoint.ps1

--- SysScripts/External SharePoint Site/modules/step3-sharepoint.ps1
+++ SysScripts/External SharePoint Site/modules/step3-sharepoint.ps1
@@ -258,7 +258,7 @@ function Invoke-Step3-SharePoint {
 
     # Splat shared connection params to avoid repeating them on every call
     $connParams = @{
-        TenantName      = $Config.SPTenantName
+        TenantName      = $Config.TenantName
         ClientId        = $Config.ClientId
         CertFolder      = $Config.CertFolder
         ApplicationName = $Config.ApplicationName
